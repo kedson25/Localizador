f15270ee0a0d4ed118b2a80aa86b65c78ca99a6e
KED <matheuslite0333@gmail.com>
2026-09-21 00:38:34 -0300

    security: protect list routes and restrict destructive operations

--- api/listas.ts
+++ api/listas.ts
@@ -1,10 +1,21 @@
 import listasIndexHandler from './_listas/index';
 import listaIdHandler from './_listas/[id]';
 import reconcileHandler from './_listas/reconcile';
+import { requireApproved, requireAdmin, normalizeAuthError } from './_lib/auth';
 import { sendError } from './_lib/response';
 
 export default async function handler(req: any, res: any) {
   const { action } = req.query;
+
+  try {
+    const destructive = action === 'reconcile' || (action === 'id' && req.method === 'DELETE');
+    if (destructive) await requireAdmin(req);
+    else await requireApproved(req);
+  } catch (err: any) {
+    const authError = normalizeAuthError(err);
+    return sendError(res, authError.statusCode, authError.code, authError.message);
+  }
+
   switch (action) {
     case 'index': return listasIndexHandler(req, res);
     case 'id': return listaIdHandler(req, res);
